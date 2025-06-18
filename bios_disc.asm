fc0e994a2719756be5bb065612fa3c3fc3d66618
Jim <jimsamuel50@gmail.com>
2025-06-18 19:59:38 +0100

    pre-kernel bootloader disc reading operation

---
+++ bios_disc.asm
@@ -0,0 +1,44 @@
+; computer goes into power on self test and needs
+; to load the os and kernel but the bios cannot do it
+; we delegate that task to the bootloader to handle it
+
+disc_load:
+    pusha                   ; move all registers to the stack
+    push dx                 ; using 'dx' to store extra data
+                            ; 'dh' stores sector number 'dl' stores drive number
+
+    mov al, dh              ; how many sectors do we want to read
+    mov cl, 0x02            ; start reading from the second sector first sector is the boot sector
+    mov ch, 0x00            ; cylinder number for current sector
+    mov dh, 0x00            ; disc head number for current sector
+
+    int 0x13
+    jc disc_error           ; if there's a carry flag there's an error
+
+    pop dx                  ; return the number of sectors read
+    cmp al, dh              ; is it equal to what we have in 'al'
+    jne sector_error        ; if not equal it's a sector error
+
+    popa 
+    ret 
+
+disc_error:
+    mov bx, DISC_ERRORS      ; routine to read disk errors and return them
+    call print 
+    call print_nl
+    mov dh, ah 
+
+    popa                     ; free all the registers from the stack memory
+    ret 
+
+; routine to read the errors from the sector
+; if the numbers read are != sector number in 0x02
+sector_error:
+    mov bx, SECTOR_ERRORS 
+    call print 
+
+DISC_ERRORS: db 'error reading from disc', 0 
+SECTOR_ERRORS: db 'invalid number of sectors', 0
+
+times 510-($-$$) db 0   ; bootloader needs 512 bytes of mem. so we pad the rest
+dw 0xaa55               ; memory location offset for bootloader
