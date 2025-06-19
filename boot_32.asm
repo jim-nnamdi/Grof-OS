fa6545109c079ef94de96a59ede1bc006260f815
Jim <jimsamuel50@gmail.com>
2025-06-19 01:06:02 +0100

    kernel initialisation from bootloader and bios

--- boot_32.asm
+++ boot_32.asm
@@ -1,25 +1,25 @@
 [org 0x7c00]
 
     mov bp, 0x90000             ; set offset for stack pointer
-    mov sp, bp 
+    mov sp, bp                  ; move stack pointer to 'bp'
 
-    mov bx, MSG_REAL_MODE 
-    call print 
+    mov bx, MSG_REAL_MODE       ; specify real mode entry
+    call print                  ; bios prnt scr routine
     call print_nl 
 
     call switch_to_pm           ; convert to protected mode
 
     jmp $                       ; its never executed
 
-%include "./bios_print.asm"
+%include "./bios_print.asm"     
 %include "./boot_switch.asm"
 %include "./boot_32_print.asm"
 
-[bits 32]
-begin_pm:
-    mov ebx, MSG_PROT_MODE
-    call print_pm
-    jmp $
+[bits 32]                         ; begin entry to PM
+begin_pm:                         ; label called in gdt routine
+    mov ebx, MSG_PROT_MODE        ; specify the data display mode
+    call print_pm                 ; protected mode routine
+    jmp $                         
 
 MSG_REAL_MODE db 'switching to real mode', 0 
 MSG_PROT_MODE db ' switching to protected mode', 0    
---
+++ boot_sect.asm
@@ -0,0 +1,52 @@
+; this calls various bios routines and specifies
+; different entry points to load the kernel based
+; on previous routines
+
+[org 0x7c00]                    ; specify memory read offset
+kernel_offset   equ 0x1000      ; kernel offset for memory
+
+    mov [boot_drive], dl        ; dl holds drive number from disc
+    mov bp, 0x90000             ; load stack memory location
+    mov sp, bp                  ; stack now points to '0x90000'
+
+    mov bx, MSG_LOAD_REAL       ; Loading 16-bit real mode
+    call print                  ; display info to user from bios
+    call print_nl 
+
+    call load_kernel            ; load the kernel from this call
+    call switch_to_pm           ; after kernel loads, switch to PM
+    jmp $                       ; never executed ...
+
+%include './bios_print.asm'     ; bios routine for 16-bit print
+%include './bios_hex.asm'       ; bios routine for hex conversion
+%include './bios_disc.asm'      ; bios routine for disk reads
+%include './bios_gdt.asm'       ; global descriptor routine
+%include './boot_32_print.asm'  ; 32-bit printing mode
+%include './boot_switch.asm'    ; code and data segmentation
+
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERN       ; bios routine to declare k-mode
+    call print 
+    call print_nl
+    
+    mov bx, kernel_offset       ; read data from disc to kernel
+    mov dl, [boot_drive]        ; read data from drive to 'dl'
+    mov dh, 2                   ; no of sectors to read
+    call disc_load              ; Load data from disc to kernel
+    ret
+
+[bits 32]
+begin_pm:                       ; Loading data in 32 bit mode
+    mov ebx, MSG_LOAD_PROT      ; inform user of 32 bit switch
+    call print_pm               ; protected mode call 
+    call kernel_offset          ; call mem at kernel linker
+    jmp $ 
+
+boot_drive db 0 
+MSG_LOAD_KERN db 'loading kernel', 0
+MSG_LOAD_PROT db 'loading 32-bit protected mode', 0
+MSG_LOAD_REAL db 'loading 16-bit real mode', 0 
+
+times 510 - ($-$$) db 0 
+dw 0xaa55
